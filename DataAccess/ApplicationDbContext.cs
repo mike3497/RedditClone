1eb1ba1d49c219455faaf4575adfb8c5332a2db8
mike3497 <mcarillon97@gmail.com>
2020-01-22 21:23:36 -0500

    Add project files.

---
+++ DataAccess/ApplicationDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Common.Models;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext()
+            : base("DefaultConnection", throwIfV1Schema: false)
+        {
+        }
+
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+    }
+}
